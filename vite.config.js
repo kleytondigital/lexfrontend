d8421fd1755378ce47a2c5b55962814dbca239ce
Kleyton Gonçalves <eu.kleytongoncalves@gmail.com>
2025-01-30 14:43:54 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
         port: 3001,
         host: '0.0.0.0',
-        preview: {
-            allowedHosts: [
-                'lex.aoseudispor.com.br',
-                'localhost',
-                '185.208.206.76',
-                '0.0.0.0', // Adicionar esse IP pode ajudar em alguns cenários
-            ],
-        },
         proxy: {
             '/api': {
                 target: 'http://lex.aoseudispor.com.br:5000',
@@ -22,5 +13,13 @@ export default defineConfig({
                 secure: false,
             },
         },
+    },
+    preview: {
+        allowedHosts: [
+            'lex.aoseudispor.com.br',
+            'localhost',
+            '185.208.206.76',
+            '0.0.0.0',
+        ],
     },
 });
